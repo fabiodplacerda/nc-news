ef6dfaadc83b66248db9270172396cacd4226021
fabiodplacerda <lacerda96@gmail.com>
2023-11-20 16:05:16 +0000

    created endpoint, controller, models for articles and started tests for getting articles by Id

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 // Controllers
 const { getTopics } = require('./Controllers/topics-controller');
+const { getArticleById } = require('./Controllers/articles-controller');
 
 //Error Handlers
 const { handleServerErrors } = require('./errors');
@@ -10,6 +11,8 @@ const app = express();
 
 //Endpoints
 app.get('/api/topics', getTopics);
+
+app.get('/api/articles/:article_id', getArticleById);
 
 app.use(handleServerErrors);
 
