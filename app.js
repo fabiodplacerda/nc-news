7cb333b1801bf2b888a9e23efdc14a2c7ebab27a
fabiodplacerda <lacerda96@gmail.com>
2023-12-11 10:17:41 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 //Error Handlers
 const {
